726bf54eb5b1a45db076f02f3dd571ac2810fa5b
corentingobbo <corentin.gobbo@gmail.com>
2025-11-27 00:09:41 +0100

    fix + Dockerfile

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,21 +1,15 @@
-const parse = require("pg-connection-string").parse;
-const config = parse(process.env.DATABASE_URL);
-
 module.exports = ({ env }) => ({
   connection: {
     client: "postgres",
     connection: {
-      client: "postgres",
-      connection: {
-        host: env("DATABASE_HOST"),
-        port: env.int("DATABASE_PORT"),
-        database: env("DATABASE_NAME"),
-        user: env("DATABASE_USERNAME"),
-        password: env("DATABASE_PASSWORD"),
-        ssl: {
-          rejectUnauthorized: false,
-        },
-      },
+      host: env("DATABASE_HOST"),
+      port: env.int("DATABASE_PORT"),
+      database: env("DATABASE_NAME"),
+      user: env("DATABASE_USERNAME"),
+      password: env("DATABASE_PASSWORD"),
+      ssl: env.bool("DATABASE_SSL", false) ? {
+        rejectUnauthorized: env.bool("DATABASE_SSL_REJECT_UNAUTHORIZED", false),
+      } : false,
     },
     debug: false,
   },
--- config/env/production/plugin.js
+++ config/env/production/plugin.js
@@ -13,4 +13,9 @@ module.exports = ({ env }) => ({
         },
       },
     },
+    'users-permissions': {
+      config: {
+        jwtSecret: env('JWT_SECRET'),
+      },
+    },
   });
