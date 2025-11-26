36b5d6292bd4376f3cb5b89993f0f0785a298726
corentingobbo <corentin.gobbo@gmail.com>
2025-11-27 00:28:57 +0100

    fix + Dockerfile

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,7 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'), // INDISPENSABLE : permet d'écouter l'extérieur
   port: env.int('PORT', 1337),  // Bonnes pratiques
-  url: env('MY_URL'),           // Votre URL publique
+  url: env('MY_HEROKU_URL'),           // Votre URL publique
   app: {
     keys: env.array('APP_KEYS'), // Sécurité requise pour Strapi v4
   },
