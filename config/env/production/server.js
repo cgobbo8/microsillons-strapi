da60ef5f9f64377bad5361016e8f52f1b0b13bc9
corentingobbo <corentin.gobbo@gmail.com>
2025-11-27 00:28:25 +0100

    fix + Dockerfile

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,3 +1,11 @@
 module.exports = ({ env }) => ({
-    url: env('MY_HEROKU_URL'),
-  });
+  host: env('HOST', '0.0.0.0'), // INDISPENSABLE : permet d'écouter l'extérieur
+  port: env.int('PORT', 1337),  // Bonnes pratiques
+  url: env('MY_URL'),           // Votre URL publique
+  app: {
+    keys: env.array('APP_KEYS'), // Sécurité requise pour Strapi v4
+  },
+  webhooks: {
+    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
+  },
+});
