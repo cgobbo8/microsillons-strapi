586734e4d0dd2d9ac95e66b805f881118227cdcf
corentingobbo <corentin.gobbo@gmail.com>
2025-11-26 23:59:19 +0100

    fix + Dockerfile

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -7,11 +7,11 @@ module.exports = ({ env }) => ({
     connection: {
       client: "postgres",
       connection: {
-        host: env("DATABASE_HOST", "containers-us-west-129.railway.app"),
-        port: env.int("DATABASE_PORT", 6616),
-        database: env("DATABASE_NAME", "railway"),
-        user: env("DATABASE_USERNAME", "postgres"),
-        password: env("DATABASE_PASSWORD", "VgmyOvM8hCHbgPxZpIfS"),
+        host: env("DATABASE_HOST"),
+        port: env.int("DATABASE_PORT"),
+        database: env("DATABASE_NAME"),
+        user: env("DATABASE_USERNAME"),
+        password: env("DATABASE_PASSWORD"),
         ssl: {
           rejectUnauthorized: false,
         },
