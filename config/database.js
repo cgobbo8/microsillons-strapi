81d084977b1aba5ff647081e8014b48dd4cd0cc1
corentingobbo <corentin.gobbo@gmail.com>
2026-01-13 23:17:19 +0100

    fix SSL cert

--- config/database.js
+++ config/database.js
@@ -7,7 +7,9 @@ module.exports = ({ env }) => ({
       database: env("DATABASE_NAME"),
       user: env("DATABASE_USERNAME"),
       password: env("DATABASE_PASSWORD"),
-      ssl: env.bool("DATABASE_SSL"),
+      ssl: env.bool("DATABASE_SSL") ? {
+        rejectUnauthorized: false,
+      } : false,
     },
   },
 });
